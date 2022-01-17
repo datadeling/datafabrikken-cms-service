8878a136729c5b1bf6a7ae0f41c23dfbd8a63973
Jeff Reiffers <jeffreiffers@gmail.com>
2022-01-17 12:29:54 +0100

    fix: added missing comma

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://cms.datafabrikken.norge.no'
+  url: 'https://cms.datafabrikken.norge.no',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET')
